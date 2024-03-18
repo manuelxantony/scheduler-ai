b33ea8a77d34a20c3b7a9bf3a49ac7de513b279a
manuelxantony <manuelxantony@gmail.com>
2024-03-19 00:55:16 +0530

    removed toggle button from page

--- app/dashboard/availability/page.tsx
+++ app/dashboard/availability/page.tsx
@@ -1,11 +1,9 @@
 import AvailabilityForm from '@/components/availability/availabilityForm';
-import ToggleButton from '@/components/button/toggleButton';
 
 const AvailablityPage = () => {
   return (
     <div>
       <AvailabilityForm />
-      <ToggleButton />
     </div>
   );
 };
